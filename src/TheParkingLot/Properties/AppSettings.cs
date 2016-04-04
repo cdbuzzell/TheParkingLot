6609025271dda053158c375e9f2a6d56576f2c4a
Corey Buzzell <cdbuzzell@gmail.com>
2016-04-03 22:11:09 -0500

    AppSettings for photos, twitter. Added git files to soln.

--- src/TheParkingLot/Properties/AppSettings.cs
+++ src/TheParkingLot/Properties/AppSettings.cs
@@ -5,5 +5,7 @@
         public string SiteTitle { get; set; }
         public int CurrentSeason{ get; set; }
         public int FirstSeason { get; set; }
+        public string PhotosUrl { get; set; }
+        public string TwitterHandle { get; set; }
     }
 }
