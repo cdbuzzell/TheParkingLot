0fa0c552e104d42839a9d8cfb1455243fdef88b9
Corey Buzzell <cdbuzzell@gmail.com>
2016-04-05 16:56:10 -0500

    Replaced Google Analytics with Azure AppInsight instrumentation

--- src/TheParkingLot/Properties/AppSettings.cs
+++ src/TheParkingLot/Properties/AppSettings.cs
@@ -5,10 +5,10 @@
         public string SiteTitle { get; set; }
         public int CurrentSeason{ get; set; }
         public int FirstSeason { get; set; }
+        public string InstrumentationKey { get; set; }
         public string PhotosUrl { get; set; }
         public string TwitterUsername { get; set; }
         public string TwitterWidgetId { get; set; }
-        public string GoogleAnalyticsTrackingId { get; set; }
         public string WeatherDefaultLat { get; set; }
         public string WeatherDefaultLong { get; set; }
         public string WeatherDefaultName { get; set; }
